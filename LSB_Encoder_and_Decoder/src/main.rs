473d8f22e1b3735843c4da73387819a2502fbae5
slanja <97552526+slanja@users.noreply.github.com>
2024-01-15 16:11:57 +0100

    working algorithm for increasing value of each color and alfa channel

--- LSB_Encoder_and_Decoder/src/main.rs
+++ LSB_Encoder_and_Decoder/src/main.rs
@@ -1,13 +1,14 @@
 use image::{GenericImage, GenericImageView, Rgba, RgbaImage};
 
 fn main() {
-    test()
+    encode()
 }
 
-fn test() {
-
+fn encode() {
+    // input image
     let img_input = image::open("images/crab.png").expect("File not found!");
 
+    // creating empty image
     let mut new_img = RgbaImage::new(img_input.width(), img_input.height());
 
     // message you want to hide
@@ -18,14 +19,47 @@ fn test() {
 
     // converting message to binary representation
     for character in message.clone().into_bytes() {
-        message_in_binary += &format!("0{:b} ", character);
+        message_in_binary += &format!("0{:b}", character);
     }
     println!("\"{}\" in binary is {}", message, message_in_binary);
 
     // replacing each pixel in image with our message
+    let mut i = 0;
+
     for (x, y, pixel) in img_input.pixels() {
-        let new_pixel_value = Rgba([255, 255, 0, 255]);
+        let mut new_pixel_value = img_input.get_pixel(x, y);;
+
+        // red color
+        if  new_pixel_value.0[0] % 2 == 0 {
+            if new_pixel_value.0[0] < 255 {
+                new_pixel_value.0[0] += 1;
+            } else { new_pixel_value.0[0] -= 1;}
+        }
+
+        // green color
+        if  new_pixel_value.0[1] % 2 == 0 {
+            if new_pixel_value.0[1] < 255 {
+                new_pixel_value.0[1] += 1;
+            } else { new_pixel_value.0[1] -= 1;}
+        }
+
+        // blue color
+        if  new_pixel_value.0[2] % 2 == 0 {
+            if new_pixel_value.0[2] < 255 {
+                new_pixel_value.0[2] += 1;
+            } else { new_pixel_value.0[2] -= 1;}
+        }
+
+        // alfa channel
+        if  new_pixel_value.0[3] % 2 == 0 {
+            if new_pixel_value.0[3] < 255 {
+                new_pixel_value.0[3] += 1;
+            } else { new_pixel_value.0[3] -= 1;}
+        }
+
         new_img.put_pixel(x, y, new_pixel_value);
+        println!("{:?}", new_pixel_value);
+        i += 1;
     }
 
     new_img.save("images/output.png").expect("Failed to save new image.");
