c32d107e2a47e5fd25927b5e06be518d6a288fd5
slanja <97552526+slanja@users.noreply.github.com>
2024-01-17 17:11:38 +0100

    beautify the code

--- LSB_Encoder_and_Decoder/src/main.rs
+++ LSB_Encoder_and_Decoder/src/main.rs
@@ -1,13 +1,22 @@
-use std::ops::Index;
-use image::{GenericImage, GenericImageView, Rgba, RgbaImage};
+use std::io;
+use image::{GenericImage, GenericImageView, RgbaImage};
 
 fn main() {
-    decode()
+    read_img()
+}
+
+fn read_img() {
+    // input image
+    let img_input = image::open("images/testoutput.png").expect("File not found!");
+
+    for (x, y, _pixel) in img_input.pixels() {
+        println!("{:?}", _pixel)
+    }
 }
 
 fn encode() {
     // input image
-    let img_input = image::open("images/crab.png").expect("File not found!");
+    let img_input = image::open("images/test.png").expect("File not found!");
 
     // creating empty image
     let mut new_img = RgbaImage::new(img_input.width(), img_input.height());
@@ -27,35 +36,27 @@ fn encode() {
     // replacing each pixel in image with our message
     let mut i = 0;
 
-    for (x, y, pixel) in img_input.pixels() {
-        let mut new_pixel_value = img_input.get_pixel(x, y);;
+    for (x, y, _pixel) in img_input.pixels() {
+        let mut new_pixel_value = img_input.get_pixel(x, y);
 
         // red color
-        if  new_pixel_value.0[0] % 2 == 0 && message_in_binary.as_bytes()[i] == "0".parse().unwrap() && i < message_in_binary.len()-1 {
-            if new_pixel_value.0[0] < 255 {
-                new_pixel_value.0[0] += 1;
-            } else { new_pixel_value.0[0] -= 1; }
+        if  new_pixel_value.0[0] % 2 == 0 && message_in_binary.as_bytes()[i] == "1".parse().unwrap() && i < message_in_binary.len()-1 {
+            if new_pixel_value.0[0] < 255 { new_pixel_value.0[0] += 1; } else { new_pixel_value.0[0] -= 1; }
         }
 
         // green color
-        if  new_pixel_value.0[1] % 2 == 0 && message_in_binary.as_bytes()[i] == "0".parse().unwrap() && i < message_in_binary.len()-1 {
-            if new_pixel_value.0[1] < 255 {
-                new_pixel_value.0[1] += 1;
-            } else { new_pixel_value.0[1] -= 1; }
+        if  new_pixel_value.0[1] % 2 == 0 && message_in_binary.as_bytes()[i] == "1".parse().unwrap() && i < message_in_binary.len()-1 {
+            if new_pixel_value.0[1] < 255 { new_pixel_value.0[1] += 1; } else { new_pixel_value.0[1] -= 1; }
         }
 
         // blue color
-        if  new_pixel_value.0[2] % 2 == 0 && message_in_binary.as_bytes()[i] == "0".parse().unwrap() && i < message_in_binary.len()-1 {
-            if new_pixel_value.0[2] < 255 {
-                new_pixel_value.0[2] += 1;
-            } else { new_pixel_value.0[2] -= 1; }
+        if  new_pixel_value.0[2] % 2 == 0 && message_in_binary.as_bytes()[i] == "1".parse().unwrap() && i < message_in_binary.len()-1 {
+            if new_pixel_value.0[2] < 255 { new_pixel_value.0[2] += 1; } else { new_pixel_value.0[2] -= 1; }
         }
 
         // alfa channel
-        if  new_pixel_value.0[3] % 2 == 0 && message_in_binary.chars().nth(i).unwrap() == '0' && i < message_in_binary.len()-1 {
-            if new_pixel_value.0[3] < 255 {
-                new_pixel_value.0[3] += 1;
-            } else { new_pixel_value.0[3] -= 1; }
+        if  new_pixel_value.0[3] % 2 == 0 && message_in_binary.chars().nth(i).unwrap() == '1' && i < message_in_binary.len()-1 {
+            if new_pixel_value.0[3] < 255 { new_pixel_value.0[3] += 1; } else { new_pixel_value.0[3] -= 1; }
         }
 
         new_img.put_pixel(x, y, new_pixel_value);
@@ -66,34 +67,34 @@ fn encode() {
         }
     }
 
-    new_img.save("images/output.png").expect("Failed to save new image.");
+    new_img.save("images/testoutput.png").expect("Failed to save new image.");
 
 }
 
 fn decode() {
     // input image
-    let img_input = image::open("images/output.png").expect("File not found!");
+    let img_input = image::open("images/testoutput.png").expect("File not found!");
 
     // empty message in binary
     let mut message_in_binary = "".to_string();
 
-    let mut count = 0;
+    // let mut count = 0;
 
     // extracting message from image
-    for (x, y, pixel) in img_input.pixels() {
-        let mut pixel_value = img_input.get_pixel(x, y);
+    for (x, y, _pixel) in img_input.pixels() {
+        let pixel_value = img_input.get_pixel(x, y);
 
         // red color
-        if pixel_value.0[0] % 2 != 0 { message_in_binary += "1"; } else { message_in_binary += "0"; }
+        if pixel_value.0[0] % 2 == 0 { message_in_binary += "0"; } else { message_in_binary += "1"; }
 
         // green color
-        if pixel_value.0[1] % 2 != 0 { message_in_binary += "1"; } else { message_in_binary += "0"; }
+        if pixel_value.0[1] % 2 == 0 { message_in_binary += "0"; } else { message_in_binary += "1"; }
 
         // blue color
-        if pixel_value.0[2] % 2 != 0 { message_in_binary += "1"; } else { message_in_binary += "0"; }
+        if pixel_value.0[2] % 2 == 0 { message_in_binary += "0"; } else { message_in_binary += "1"; }
 
         // alfa channel
-        if pixel_value.0[3] % 2 != 0 { message_in_binary += "1"; } else { message_in_binary += "0"; }
+        if pixel_value.0[3] % 2 == 0 { message_in_binary += "0"; } else { message_in_binary += "1"; }
 
         /*count += 1;
 
