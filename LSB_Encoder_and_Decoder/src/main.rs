d24128fc0123643f986fb42aed37c66666b3f7d3
slanja <97552526+slanja@users.noreply.github.com>
2024-01-29 21:47:47 +0100

    working on rubber duck debugging 🦆

--- LSB_Encoder_and_Decoder/src/main.rs
+++ LSB_Encoder_and_Decoder/src/main.rs
@@ -15,7 +15,7 @@ fn read_img() {
 }
 
 fn encode() {
-    // input image
+    // loading input image
     let img_input = image::open("images/test.png").expect("File not found!");
 
     // creating empty image
@@ -36,26 +36,36 @@ fn encode() {
     // replacing each pixel in image with our message
     let mut i = 0;
 
+    /*
+    íčko se nezvyšuje, Koldy help
+    */
+
     for (x, y, _pixel) in img_input.pixels() {
         let mut new_pixel_value = img_input.get_pixel(x, y);
 
         // red color
         if  new_pixel_value.0[0] % 2 == 0 && message_in_binary.chars().nth(i) == Option::from('1') && i < message_in_binary.len()-1 {
+            i += 1;
             if new_pixel_value.0[0] < 255 { new_pixel_value.0[0] += 1; } else { new_pixel_value.0[0] -= 1; }
         }
 
+        print!("{}", i);
+
         // green color
         if  new_pixel_value.0[1] % 2 == 0 && message_in_binary.chars().nth(i) == Option::from('1') && i < message_in_binary.len()-1 {
+            i += 1;
             if new_pixel_value.0[1] < 255 { new_pixel_value.0[1] += 1; } else { new_pixel_value.0[1] -= 1; }
         }
 
         // blue color
         if  new_pixel_value.0[2] % 2 == 0 && message_in_binary.chars().nth(i) == Option::from('1') && i < message_in_binary.len()-1 {
+            i += 1;
             if new_pixel_value.0[2] < 255 { new_pixel_value.0[2] += 1; } else { new_pixel_value.0[2] -= 1; }
         }
 
         // alfa channel
         if  new_pixel_value.0[3] % 2 == 0 && message_in_binary.chars().nth(i) == Option::from('1') && i < message_in_binary.len()-1 {
+            i += 1;
             if new_pixel_value.0[3] < 255 { new_pixel_value.0[3] += 1; } else { new_pixel_value.0[3] -= 1; }
         }
 
